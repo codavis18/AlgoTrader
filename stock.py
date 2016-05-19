e41259c0d4b55f956448ed742a0ab0644ec2f5a1
Nick Fiacco <nicholas.r.fiacco.17@dartmouth.edu>
2016-05-19 14:38:54 -0400

    cleaned up repository

--- stock.py
+++ stock.py
@@ -2,9 +2,12 @@
 class Stock:
 	def __init__(self, name, threshold = 5):
 		self.name = name
-		self.value = 0
+
+		# prices and values are dictionaries with key as date of tweet
+		self.prices = {}
+		self.values = {}
 		self.threshold = threshold
 
 	def __str__(self):
-		return str(self.name + " Score is " + str(self.value))
+		return str(self.name)
 
